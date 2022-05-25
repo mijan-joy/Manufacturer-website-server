6dc323ff08c94db74a963015bd7f5126b9fab705
souadhimel <souad.anam@gmail.com>
2022-05-25 15:46:17 +0600

    create added

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
 const express=require('express');
 const cors=require('cors');
 const app=express();
-const ObjectId = require("mongodb").ObjectId;
-const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port=process.env.PORT ||5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-//  middlewares
+//  middleware
 app.use(cors())
 app.use(express.json())
 
 
-const uri = "mongodb+srv://dbuser1:<password>@cluster0.j1mjs.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bv0uy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const serviceCollection = client.db("carTools").collection("service");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+await client.connect();
+const serviceCollection = client.db("carBiz").collection("service");
+
+app.get("/service", async(req, res)=>{
+const query={}
+const cursor=serviceCollection.find(query);
+const services=await cursor.toArray()
+res.send(services)
+})
+
+//..........................Create(C):(POST)...........................//
+
+// Create (POST)
+app.post("/service",async(req, res) => {
+    const serv=req.body;
+    const result=await serviceCollection.insertOne(serv);
+    res.json(result);
+
+})
+
+    }
+    finally{
+
+    }
+
+
+}
+run().catch(console.dir);
+
+
+app.get('/',(req, res) => {
+    res.send("Running my manufacturer website")
+})
+
+app.listen(port,()=>{
+    console.log("Listening on port",port);
+    })
+    
